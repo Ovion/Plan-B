a2eb62eed1548ec24fb8a48f96fd2a8edd92b20b
Ovion <ovi2825@gmail.com>
2019-12-16 12:41:11 +0100

    Primeros pasos

--- src/api/atlas_mongo/Mongo.py
+++ src/api/atlas_mongo/Mongo.py
@@ -7,3 +7,14 @@ class ConectColl:
         self.client = MongoClient(MONGO.format(USER_ATLAS, KEY_ATLAS))
         self.db = self.client['Bycicle_Accidents']
         self.acc = self.db['History']
+
+    def add_doc(self, docu):
+        self.acc.insert_one(docu)
+
+    def create(self, year, lat, lon):
+        document = {
+            'year': year,
+            'lat': lat,
+            'lon': lon
+        }
+        return self.add_doc(document)
--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from atlas_mongo.Mongo import ConectColl
 
 app = Flask(__name__)
 
@@ -8,5 +9,15 @@ def hello():
     return 'Hi Ovi!'
 
 
+@app.route('/create', methods=['POST'])
+def create():
+    year = request.args['year']
+    lat = request.args['lat']
+    lon = request.args['lon']
+    coll.create(year, lat, lon)
+    return 'Doc Inserted'
+
+
 if __name__ == '__main__':
+    coll = ConectColl()
     app.run(debug=True)
