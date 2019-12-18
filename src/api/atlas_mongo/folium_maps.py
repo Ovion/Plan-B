ed45a2456628f749687d60afcba0661cf2627457
Ovion <ovi2825@gmail.com>
2019-12-18 07:11:20 +0100

    Retocando limpieza

--- src/api/atlas_mongo/folium_maps.py
+++ src/api/atlas_mongo/folium_maps.py
@@ -14,7 +14,7 @@ def create_df_coords(datas):
         df = df.append({
             'Lat': data['localizacion']['coordinates'][1],
             'Lon': data['localizacion']['coordinates'][0],
-            'Weights': 0.20 if data['lesividad'] == 'Leve' else (0.80 if data['lesividad'] == 'Moderada' else 1)
+            'Weights': 0.2 if data['lesividad'] == 'Leve' else (5 if data['lesividad'] == 'Moderada' else 10)
         }, ignore_index=True)
     return df
 
@@ -30,19 +30,44 @@ def create_map(df):
     return heat_m
 
 
-def create_map_dir(df, lat_a, lon_a, lat_b, lon_b):
+def create_map_dir(coll, lat_a, lon_a, lat_b, lon_b):
     # Create a base map
     start_lat = round((lat_a+lat_b)/2, 7)
     start_lon = round((lon_b+lon_a)/2, 7)
     heat_m = folium.Map(location=[start_lat, start_lon],
                         tiles='cartodbpositron', zoom_start=15)
 
-    # Add a heatmap to the base map
-    HeatMap(data=df[['Lat', 'Lon', 'Weights']], radius=15).add_to(heat_m)
+    # Create groups
 
+    manana_group = folium.FeatureGroup(name="Mañana")
+    tarde_group = folium.FeatureGroup(name="Tarde")
+    noche_group = folium.FeatureGroup(name="Noche")
+
+    # Add a heatmap to the base map
+    Marker([lat_a, lon_a]).add_to(heat_m)
     # Add childs
-    heat_m.add_child(Marker([lat_a, lon_a]))
     heat_m.add_child(Marker([lat_b, lon_b]))
+
+    bicis_manana = coll.acc.find({"horario": "Mañana"})
+    acc_manana = create_df_coords(bicis_manana)
+    HeatMap(data=acc_manana[['Lat', 'Lon', 'Weights']],
+            radius=15).add_to(manana_group)
+
+    bicis_tarde = coll.acc.find({"horario": "Tarde"})
+    acc_tarde = create_df_coords(bicis_tarde)
+    HeatMap(data=acc_tarde[['Lat', 'Lon', 'Weights']],
+            radius=15).add_to(tarde_group)
+
+    bicis_noche = coll.acc.find({"horario": "Noche"})
+    acc_noche = create_df_coords(bicis_noche)
+    HeatMap(data=acc_noche[['Lat', 'Lon', 'Weights']],
+            radius=15).add_to(noche_group)
+
+    manana_group.add_to(heat_m)
+    tarde_group.add_to(heat_m)
+    noche_group.add_to(heat_m)
+    folium.LayerControl(collapsed=True).add_to(heat_m)
+
     return heat_m
 
 
@@ -71,8 +96,6 @@ def print_heat_map_i(coll, injury):
 
 
 def print_heat_map_dir(coll, lat_a, lon_a, lat_b, lon_b):
-    bicis = coll.acc.find()
-    accidents = create_df_coords(bicis)
-    heat_map = create_map_dir(accidents, lat_a, lon_a, lat_b, lon_b)
+    heat_map = create_map_dir(coll, lat_a, lon_a, lat_b, lon_b)
     heat_map.save(f'output/heat_map.html')
     return heat_map
