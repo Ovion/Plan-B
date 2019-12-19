f90de0a42181c61842af63d7f8d78230b53ab269
Ovion <ovi2825@gmail.com>
2019-12-19 11:38:30 +0100

    bla

--- src/api/atlas_mongo/folium_maps.py
+++ src/api/atlas_mongo/folium_maps.py
@@ -87,7 +87,7 @@ def print_heat_map_dir(coll, lat_a, lon_a, lat_b, lon_b):
 def print_heat_map_pred(df, lat_a, lon_a, lat_b, lon_b):
     start_lat = round((lat_a+lat_b)/2, 7)
     start_lon = round((lon_b+lon_a)/2, 7)
-    heat_map = folium.Map(location=[40.416, -3.694],
+    heat_map = folium.Map(location=[start_lat, start_lon],
                           tiles='cartodbpositron', zoom_start=15)
     HeatMap(data=df[['lat', 'lon', 'weights']], radius=15).add_to(heat_map)
 
