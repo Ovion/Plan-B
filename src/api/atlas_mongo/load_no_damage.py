c1d1d035377f6e386186af7e68614e0afa408856
Ovion <ovi2825@gmail.com>
2020-01-15 09:57:32 +0100

    Reducidos los leves

--- src/api/atlas_mongo/load_no_damage.py
+++ src/api/atlas_mongo/load_no_damage.py
@@ -4,6 +4,30 @@ from Mongo import submit_df
 
 bici = pd.read_csv("input/clean_data/bici_clean.csv")
 
+# Here I reduce the number of accidents with injury 'Leve' in order to a better prediction
+num_lon = 1/93.75*0.5
+num_lat = 1/125*0.5
+
+bici['num_acc'] = 100
+
+for i, r in bici.loc[bici.lesividad == 'Leve'].iterrows():
+    accs = len(bici.loc[
+        (bici.horario == r['horario']) & (bici.festividad == r['festividad']) & (bici.meteo == r['meteo']) &
+        (bici.lon >= (r['lon']-num_lon)) & (bici.lon <= (r['lon']+num_lon)) &
+        (bici.lat >= (r['lat']-num_lat)) & (bici.lat <= (r['lat']+num_lat))])
+    bici.loc[i, 'num_acc'] = accs
+
+bici['mark'] = int()
+bici.loc[(bici.lesividad == 'Leve') & (bici.festividad == 'Laborable')
+         & (bici.meteo == 'Despejado') & (bici.num_acc <= 5), 'mark'] = 1
+
+bici = bici.loc[bici.num_acc > 2]
+bici = bici.loc[bici.mark == 0]
+bici.drop(['num_acc', 'mark'], axis=1, inplace=True)
+
+bici.to_csv('input/clean_data/bici_trick.csv', index=False)
+
+# Here I calculate the 'no_damage' dataframe
 lst_h = list(bici.horario.unique())
 lst_f = list(bici.festividad.unique())
 lst_m = list(bici.meteo.unique())
